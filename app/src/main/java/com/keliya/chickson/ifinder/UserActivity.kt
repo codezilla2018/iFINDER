0a67c955a75d373d2671327406b2c527c10e8f8d
chickson <erandrajayasundara@live.com>
2018-05-11 09:36:01 +0530

    linking activities

---
+++ app/src/main/java/com/keliya/chickson/ifinder/UserActivity.kt
@@ -0,0 +1,12 @@
+package com.keliya.chickson.ifinder
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class UserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+    }
+}
--- app/src/main/java/com/keliya/chickson/ifinder/UserSelectionActivity.kt
+++ app/src/main/java/com/keliya/chickson/ifinder/UserSelectionActivity.kt
@@ -1,12 +1,20 @@
 package com.keliya.chickson.ifinder
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_user_selection.*
 
 class UserSelectionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_selection)
+        admin_User.setOnClickListener { v->
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+        button.setOnClickListener { v->
+            startActivity(Intent(this,UserActivity::class.java))
+        }
     }
 }
