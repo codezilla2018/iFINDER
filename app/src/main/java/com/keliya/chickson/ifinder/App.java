1fe7df217f69063cfb6719bb37d3508bfdf66700
chickson <erandrajayasundara@live.com>
2018-05-11 12:13:09 +0530

    map styled v1.1

---
+++ app/src/main/java/com/keliya/chickson/ifinder/App.java
@@ -0,0 +1,11 @@
+package com.keliya.chickson.ifinder;
+
+/**
+ * Created by chiCkson on 5/11/18.
+ */
+
+import android.support.multidex.MultiDexApplication;
+
+public class App extends MultiDexApplication {
+    //you can leave this empty or override methods if you like so the thing is that you need to extend from MultiDexApplication
+}
